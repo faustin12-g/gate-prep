1aa737dae461e28643776a9c30f03ad8299df79f
faustin12-g <nshimiefaustinpeace@gmail.com>
2026-07-14 23:49:42 +0200

    change codes

--- httpserver.js
+++ httpserver.js
@@ -1,42 +1,47 @@
 const http = require('http')
-const fs = require('fs')
 const path = require('path')
+const fs = require('fs')
 
 const INPUT_FILE = path.join(__dirname, 'input.txt')
-const OUTPUT_FILE = path.join(__dirname, 'out.txt')
+const OUTPUT_FILE = path.join(__dirname, 'output.txt')
+
 
 const server = http.createServer((req, res)=>{
     if(req.method === 'GET')
     {
         const readStream = fs.createReadStream(INPUT_FILE, 'utf8')
-         readStream.on('open', ()=>{
+
+        readStream.on('open', ()=>{
             res.writeHead(200, {'Content-Type': 'text/plain'})
-         })
-         readStream.on('error', ()=>{
-            res.writeHead(404, {'Content-Type': 'plain/text'})
+        })
+        readStream.on('error', ()=>{
+            res.writeHead(404, {'Content-Type': 'text/plain'})
             res.end('input.txt not found')
-         })
-         readStream.pipe(res)
+        })
+        readStream.pipe(res)
     }
-    else if (req.method === 'POST'){
+    else if(req.method === 'POST')
+    {
         const writeStream = fs.createWriteStream(OUTPUT_FILE)
+
         req.pipe(writeStream)
 
         writeStream.on('finish', ()=>{
-            res.writeHead(200, {'Content-Type': 'text/plain'})
-            res.end('Data written to output.txt')
+            res.writeHead(500, { 'Content-Type': 'text/plain'})
+            res.end('Date written to input.txt')
         })
         writeStream.on('error', ()=>{
-            res.writeHead(500,  {'Content-Type': 'text/plain'})
-            res.end('Failed to write to the file')
+            res.writeHead(500, {'Content-Type': 'text/plain'})
+            res.end('Failed to post data')
         })
     }
     else{
-        res.writeHead(405, {'Content-type': 'text/plain'})
+        res.writeHead(405, {'Content-Type': 'text/plain'})
         res.end('Method not allowed')
     }
 })
 
-server.listen(3000, ()=>{
-    console.log(`HTTP File server running on port 3000`)
+const PORT = 3000
+server.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
 })
